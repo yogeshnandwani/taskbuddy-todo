c3a660bb7a89e95dbf5f29f1bdee8fe02d66def2
Yogesh Nandwani <yogeshnandwani@Yogeshs-Mac-mini-2.local>
2026-03-16 19:39:18 +0530

    add delete task feature

---
+++ .history/js/script_20260316192507.js
@@ -0,0 +1,27 @@
+const input = document.getElementById("taskInput")
+const addBtn = document.getElementById("addBtn")
+const list = document.getElementById("taskList")
+
+addBtn.addEventListener("click", addTask)
+
+function addTask(){
+    const taskText = input.value.trim()
+
+    if(taskText === "") return
+
+    const li = document.createElement("li")
+
+    li.innerHTML = `
+    <span>${taskText}</span>
+    <button class="delete"><img src="images/delete.png" alt="Delete icon"></button>
+    `
+
+    list.appendChild(li)
+
+    list.addEventListener("click", function(e){
+        if(e.target.classList.contains("delete")){
+            e.target.parentElement.remove()
+        }
+    })
+    input.value=""
+}
